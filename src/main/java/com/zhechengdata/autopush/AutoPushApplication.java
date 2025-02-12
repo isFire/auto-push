ed7720c690d91f9ab7d0d60f6f0c0e239087134f
李昭 <lizhao@zhechengdata.com>
2025-02-12 15:53:34 +0800

    feat: 添加基本的 Spring Boot项目结构和配置
    - 创建了基本的 Spring Boot 项目结构，包括必要的目录和文件
    - 添加了基本的配置文件，如 application.properties 和 build.gradle
    - 创建了主应用类 AutoPushApplication 和测试类 AutoPushApplicationTests
    - 添加了示例控制器 BasicController 和 PathVariableController
    - 创建了示例实体类 User
    - 添加了静态 HTML 文件 index.html
    - 创建了 .gitignore 文件，忽略了常见的项目构建和 IDE 相关文件

---
+++ src/main/java/com/zhechengdata/autopush/AutoPushApplication.java
@@ -0,0 +1,13 @@
+package com.zhechengdata.autopush;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AutoPushApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AutoPushApplication.class, args);
+	}
+
+}
